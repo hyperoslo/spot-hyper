22eade9b3dfc6729f309d3fccbb404af8f4be814
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-12-30 17:34:00 +0430

    Add index

--- db/migrate/20160104070228_create_spots.rb
+++ db/migrate/20160104070228_create_spots.rb
@@ -1,12 +1,12 @@
 class CreateSpots < ActiveRecord::Migration[5.0]
   def change
     create_table :spots do |t|
-      t.string :name
       t.text :text
       t.string :user_name
       t.integer :user_id
-      t.text :tags, array: true, default: []
+      t.string :tags, array: true, default: []
       t.timestamps
     end
+    add_index  :spots, :tags, using: 'gin'
   end
 end
