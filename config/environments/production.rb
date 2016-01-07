9ab5bb593e480579b57728aec8f01de52da1895b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-07 06:22:10 +0000

    Change socket from secure to insecure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'wss://spot.hyper.no/cable'
+  config.action_cable.url = 'ws://spot.hyper.no/cable'
   config.action_cable.allowed_request_origins = [ 'http://spot.hyper.no', /http:\/\/localhost.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
